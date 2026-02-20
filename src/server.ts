515aa39b00b655c2055c076ce53b337eeca9c9a3
Mani Teja <maniteja2601@gmail.com>
2026-02-20 19:16:54 +0530

    Refactor server configuration to use environment variable for port. This change allows for more flexible deployment configurations by defaulting to port 3000 if the environment variable is not set.

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import { buildApp } from "./app";
 
 const app = buildApp();
 
-app.listen({ port: 3000 , host : "0.0.0.0" }, (err, address) => {
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen({ port: PORT , host : "0.0.0.0" }, (err, address) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
